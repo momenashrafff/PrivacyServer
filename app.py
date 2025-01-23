fdd9bb63647b26353208f713a975cb025de5f7f8
Mo'men Ashraf <momenashraf742@gmail.com>
2025-01-23 11:37:59 +0200

    Update app.py
    Production

--- app.py
+++ app.py
@@ -517,4 +517,4 @@ def swagger():
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 4444))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
